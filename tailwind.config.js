2b1d0641dfacf49e54b395dd4f92602c0c1a6623
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-17 15:50:22 +0530

    added Inter font and gradient color and applied it entirely

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["inter", "sans-serif"],
+      },
+      colors: {
+        supabaseGreen: "#3ECF8E",
+        darkBlack: "#0f0f0f",
+        darkGray: "#1c1c1c",
+      },
+      backgroundImage: {
+        "supabase-gradient":
+          "linear-gradient(to Bottom, #0f0f0f, #1c1c1c,#1c4c1c,rgb(5, 134, 76))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
