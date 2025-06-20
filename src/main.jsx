5ee72eb28ec7357f9cdd518b900d6f4fda7d5ab7
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-20 12:51:01 +0530

    added contextprovider to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { router } from './routes/router.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContextProvider>
+<h1 className="text-center text-2xl font-bold pt-4">Supabase Authentication System</h1>
+<RouterProvider router={router}/>
+    </AuthContextProvider>
   </StrictMode>,
 )
