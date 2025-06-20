668404a22ce7ed50c3ef19b378ba4f799c8564d2
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-20 13:00:33 +0530

    updated supabaseclient with backend url and key

--- src/context/SuperBaseClient.js
+++ src/context/SuperBaseClient.js
@@ -1,11 +1,8 @@
 // src/supabaseClient.js or supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-const supabaseUrl ="https://gxzujiinmhqfnysuqszt.supabase.co";
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4enVqaWlubWhxZm55c3Vxc3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNjg1NTgsImV4cCI6MjA2NTY0NDU1OH0.laNyDdlZnAJ1nnGTO4xn7igNQeH2wVUQXVUiC1SLVlI";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
