2a7c481d3ba630abce1b3172fff120332dee250b
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-21 23:58:19 +0530

    updated page with modularcomponents and credential validation logic

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,71 +1,79 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userAuth } from "../context/AuthContext";
-import { Eye, EyeOff } from "lucide-react";
+import EmailInput from "../components/common/EmailInput";
+import PasswordInput from "../components/common/PasswordInput";
+import { validateEmail, validatePassword } from "../utils/validators";
 
 const SignUp = () => {
+  // states
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [load, setLoad] = useState();
-  const [error, setError] = useState("");
-    const [visibility,setVisibility]=useState(false)
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const {signUpNewUser } = userAuth();
-
-  console.log(`[email:${email},password:${password}]`)
-
-
-
+  const { signUpNewUser } = userAuth();
   const navigate = useNavigate();
 
-  const handleSignUp = async (e) => {
+  //handling form submit
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoad(true);
+    const emailErr = validateEmail(email);
+    const passwordErr = validatePassword(password);
+    setEmailError(emailErr);
+    setPasswordError(passwordErr);
+
+    if (emailErr || passwordErr) return;
+
+    setLoading(true);
     try {
       const result = await signUpNewUser(email, password);
       if (result.success) {
-        navigate("/dashbaord");
-        alert(email + "sign up successfuly");
+        navigate("/dashboard");
+        alert("Sign-up successful!");
       } else {
-        setError(result.error.message);
+        alert(result.error.message);
       }
-    } catch (error) {
-      setError("An unexpected error occurred.",error); // Catch unexpected errors
+    } catch (err) {
+      console.error("Unexpected error", err);
     } finally {
-      setLoad(false);
+      setLoading(false);
     }
   };
 
-const handleVisibility=()=>{
-  setVisibility(!visibility)
-}
-
   return (
-    <div className="max-w-sm sm:max-w-md min-h-96 m-auto mt-24 md:max-w-lg shadow-md shadow-green-200 hover:shadow-green-400 p-6">
-      <form onSubmit={handleSignUp}>
+    <div className="max-w-sm sm:max-w-md min-h-96 m-auto mt-16 md:max-w-lg shadow-md shadow-green-200 hover:shadow-green-400 p-6">
+      <form onSubmit={handleSubmit}>
         <div className="mx-1">
-        <h3 className="text-md">Sign up now!</h3>
-        <p className="text-md mt-2">
-          Already have an Account? <span className="mx-2 text-indigo-400 active:text-indigo-100" ><Link to="/signin">Signin!</Link> </span>
-        </p>
+          <h3 className="text-md">Sign up now!</h3>
+          <p className="text-md mt-2">
+            Already have an Account?
+            <span className="mx-2 text-indigo-400 active:text-indigo-100">
+              <Link to="/signin">Signin!</Link>
+            </span>
+          </p>
         </div>
 
-        <div className="flex flex-col gap-6 pt-4 relative">
-          <input
-            type="email"
+        <div className="flex flex-col gap-6 mt-6">
+          {/* importing Email filed */}
+          <EmailInput
             onChange={(e) => setEmail(e.target.value)}
-  className="text-zinc-800 p-2 rounded-md text-lg font-bold"
+            error={emailError}
           />
-          <input
-            type={visibility?"text":"password"}
+          {/* importing Email filed */}
+          <PasswordInput
             onChange={(e) => setPassword(e.target.value)}
-  className="text-zinc-800 p-2 rounded-md text-lg"/>
-          <button onClick={handleVisibility} className="absolute top-24 right-3 text-black font-bold">
-               {visibility?<Eye/>:<EyeOff/>}  
+            error={passwordError}
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full text-md md:text-xl mt-6 bg-green-400 p-2 font-bold rounded hover:bg-green-600"
+          >
+            Sign Up
           </button>
-          <button type="submit" disabled={load} className="p-2 mt-6 bg-green-400 font-bold text-xl rounded-md hover:bg-green-300">Sign Up</button>
         </div>
-         {/* {error&&<p>Something went wrong!{error}<p/>} */}
       </form>
     </div>
   );
