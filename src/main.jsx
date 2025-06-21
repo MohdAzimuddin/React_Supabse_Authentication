3d80dd335d79fa555da57d268950b758cfaf18f8
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-21 15:42:04 +0530

    updated it with new title

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import './index.css'
 import { router } from './routes/router.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { Power } from 'lucide-react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
-<h1 className="text-center text-2xl font-bold pt-4">Supabase Authentication System</h1>
+            <div className="py-8 flex items-center justify-center gap-1">
+              <span className="text-green-600">
+                <Power/>
+              </span>
+              <span className="text-green-400 text-sm font-medium">
+                Powered by Supabase
+              </span>
+          </div>
 <RouterProvider router={router}/>
     </AuthContextProvider>
   </StrictMode>,
