6573f50b665c513b0d21d9cf05c9a4c5305c96cd
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-21 23:58:36 +0530

    updated page with modularcomponents and credential validation logic

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,72 +1,76 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userAuth } from "../context/AuthContext";
-import { Eye, EyeOff } from "lucide-react";
+import EmailInput from "../components/common/EmailInput";
+import PasswordInput from "../components/common/PasswordInput";
+import { validateEmail, validatePassword } from "../utils/validators";
 
 const Signin = () => {
+  // states
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [load, setLoad] = useState();
-  const [error, setError] = useState("");
-  const [visibility,setVisibility]=useState(false)
-
-  console.log(`[email:${email},password:${password}]`)
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const { signInUser } = userAuth();
   const navigate = useNavigate();
 
-  const handleSignIn = async (e) => {
-    e.preventDefault();
-    setLoad(true);
+  // handling form submit
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const emailErr = validateEmail(email);
+    const passwordErr = validatePassword(password);
+    setEmailError(emailErr);
+    setPasswordError(passwordErr);
+
+    if (emailErr || passwordErr) return;
+
+    setLoading(true);
     try {
       const result = await signInUser(email, password);
       if (result.success) {
-      navigate("/dashboard");
-        alert(email + "successfullly sigined in");
+        navigate("/dashboard");
+        alert("Signed in successfully!");
       } else {
-        setError(result.error.message);
-        console.log(result.error)
+        alert(result.error.message);
       }
-    } catch (error) {
-      console.log("An unexpected error occurred.", error); // Catch unexpected errors
+    } catch (err) {
+      console.error("Unexpected error", err);
     } finally {
-      setLoad(false);
+      setLoading(false);
     }
   };
 
-  const handleVisibility=()=>{
-    setVisibility(!visibility)
-  }
-
   return (
-  <div className="max-w-sm sm:max-w-md min-h-96 m-auto mt-24 md:max-w-lg shadow-md shadow-green-200 hover:shadow-green-400 p-6">
-      <form onSubmit={handleSignIn}>
-
+    <div className="max-w-sm sm:max-w-md min-h-96 m-auto mt-16 md:max-w-lg shadow-md shadow-green-200 hover:shadow-green-400 p-6">
+      <form onSubmit={handleSubmit}>
         <div className="mx-1">
-        <h3 className="text-md">Sign in now!</h3>
-        <p className="text-md mt-2">
-          Don't have an Account?<span className="mx-2 text-indigo-400 active:text-indigo-100">
-         <Link to="/signup">Signup!</Link></span> 
-        </p>
+          <h3 className="text-md">Sign in now!</h3>
+          <p className="text-md mt-2">
+            Don't have an Account?
+            <span className="mx-2 text-indigo-400 active:text-indigo-100">
+              <Link to="/signup">Signup!</Link>
+            </span>
+          </p>
         </div>
-
-        <div className="flex flex-col gap-6 pt-4 relative">
-          <input
-            type="email"
+        <div className="flex flex-col gap-6 mt-6">
+          {/* import email field */}
+          <EmailInput
             onChange={(e) => setEmail(e.target.value)}
-            className="text-zinc-800 p-2 rounded-md text-lg font-bold"
+            error={emailError}
           />
-          <input
-            type={visibility?"text":"password"}
+          {/* import password filed */}
+          <PasswordInput
             onChange={(e) => setPassword(e.target.value)}
-            className="text-zinc-800 p-2 rounded-md text-lg"
-            />
-            
-            <button onClick={handleVisibility} className="absolute top-24 right-3 text-black font-bold">{visibility?<Eye/>:<EyeOff/>}</button>
-          
-          <button type="submit" disabled={load}
-          className="p-2 mt-6 bg-green-400 font-bold text-xl rounded-md hover:bg-green-300">
+            error={passwordError}
+          />
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full text-md md:text-xl mt-6 bg-green-400 p-2 font-bold rounded hover:bg-green-600"
+          >
             Sign In
           </button>
         </div>
