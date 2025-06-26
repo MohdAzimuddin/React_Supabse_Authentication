cfdbb99b4301ed5e00c0aac0b291af0fa07d04f8
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-26 13:20:24 +0530

    updated the file by fixing the refresh issue using loading state

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,7 +2,17 @@ import { Navigate } from "react-router-dom";
 import { userAuth } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const { session } = userAuth();
+  const { session,loading } = userAuth();
+
+  if(loading){
+    return(
+     <div className="flex justify-center items-center h-screen">
+      <div className="w-12 h-12 border-4 border-green-400 border-dashed rounded-full animate-spin"></div>
+     </div>
+    )
+      
+  }
+
   return <>{session ? <>{children}</> : <Navigate to="/signin" />}</>;
 };
 
