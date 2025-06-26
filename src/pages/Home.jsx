7bd7689651782350ceaeb0262212c552645c9412
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-26 13:15:31 +0530

    formatted the code with pritter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { UserPlus, Lock, Shield, ArrowRight, LogIn,} from "lucide-react";
+import { UserPlus, Lock, Shield, ArrowRight, LogIn } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -27,73 +27,72 @@ const Home = () => {
   ];
 
   return (
-      <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
+      {/* Main*/}
+      <main className="flex items-center justify-center px-4">
+        <div className="max-w-5xl mx-auto text-center">
+          {/* title */}
+          <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-8">
+            <span className="block text-white mb-2">Welcome to the</span>
+            <span className="text-green-500">Authentication System</span>
+          </h1>
 
-        {/* Main*/}
-        <main className="flex items-center justify-center px-4">
-          <div className="max-w-5xl mx-auto text-center">
-            {/* title */}
-            <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-8">
-              <span className="block text-white mb-2">Welcome to the</span>
-              <span className="text-green-500">
-                Authentication System
-              </span>
-            </h1>
+          <p className="text-xl text-gray-300 max-w-3xl mx-auto mb-12">
+            Experience secure, seamless authentication with A-grade security and
+            modern design principles.
+          </p>
 
-            <p className="text-xl text-gray-300 max-w-3xl mx-auto mb-12">
-              Experience secure, seamless authentication with A-grade
-              security and modern design principles.
-            </p>
+          {/* Card */}
+          <div className="grid md:grid-cols-3 gap-8 py-16">
+            {features.map((feature) => {
+              const IconComponent = feature.icon;
+              return (
+                <div
+                  key={feature.id}
+                  className="flex flex-col items-center gap-4 shadow-md shadow-green-500 p-6"
+                >
+                  {/* Icon */}
+                  <div className="">
+                    <IconComponent className="w-6 h-6 text-white" />
+                  </div>
 
-            {/* Card */}
-            <div className="grid md:grid-cols-3 gap-8 py-16">
-              {features.map((feature) => {
-                const IconComponent = feature.icon;
-                return (
-                    <div key={feature.id} className="flex flex-col items-center gap-4 shadow-md shadow-green-500 p-6">
-                      {/* Icon */}
-                      <div className="">
-                        <IconComponent className="w-6 h-6 text-white" />
-                      </div>
+                  {/* Title */}
+                  <h3 className="text-xl font-semibold text-white mb-3">
+                    {feature.title}
+                  </h3>
 
-                      {/* Title */}
-                      <h3 className="text-xl font-semibold text-white mb-3">
-                        {feature.title}
-                      </h3>
+                  {/* Description */}
+                  <p className="text-gray-400">{feature.description}</p>
+                </div>
+              );
+            })}
+          </div>
 
-                      {/* Description */}
-                      <p className="text-gray-400">{feature.description}</p>
-                    </div>
-                );
-              })}
-            </div>
-
-            {/* Action */}
-            <div className="w-full flex flex-col md:flex-row justify-center items-center gap-6 mb-6">
-              <Link
-                to="/signup"
-                className="p-3 bg-green-400 font-bold text-xl rounded-md hover:bg-green-600"
-              >
-                <div className="flex items-center">
+          {/* Action */}
+          <div className="w-full flex flex-col md:flex-row justify-center items-center gap-6 mb-6">
+            <Link
+              to="/signup"
+              className="p-3 bg-green-400 font-bold text-xl rounded-md hover:bg-green-600"
+            >
+              <div className="flex items-center">
                 Get Started
                 <ArrowRight className="w-5 h-5 ml-2 hover:translate-x-1 transition-transform duration-600" />
-                </div>
-              </Link>
-         {/*  */}
-              <Link
-                to="/signin"
-                className="p-3 bg-transparent border-2 border-green-600 font-bold text-xl rounded-md hover:bg-green-600"
-              >
-                <div className="flex items-center">
+              </div>
+            </Link>
+            {/*  */}
+            <Link
+              to="/signin"
+              className="p-3 bg-transparent border-2 border-green-600 font-bold text-xl rounded-md hover:bg-green-600"
+            >
+              <div className="flex items-center">
                 Sign In
                 <LogIn className="w-5 h-5 ml-2 hover:translate-x-1 transition-transform duration-300" />
-                </div>
-              </Link>
-            </div>
+              </div>
+            </Link>
           </div>
-        </main>
-      </div>
-
+        </div>
+      </main>
+    </div>
   );
 };
 
