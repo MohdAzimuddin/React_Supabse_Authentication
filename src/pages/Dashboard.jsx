63269ccb6982f2595d0340679876ca55b37eff7f
MohdAzimuddin <sfsuper2020@gmail.com>
2025-06-26 13:15:16 +0530

    formatted the code with pritter

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { userAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
   const { session, signOutUser } = userAuth();
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   const handleSignOut = async (e) => {
     e.preventDefault();
@@ -12,21 +13,29 @@ const Dashboard = () => {
       const response = await signOutUser();
       if (response.success) {
         navigate("/");
-        alert("user loged out successfullly")
+        toast.success("loged out successfullly");
       }
     } catch (error) {
       console.error("error while signing out", error);
+      toast.error("something went wrong")
     }
   };
 
   return (
     <div className="flex flex-col items-center justify-center gap-6">
       <h3 className="mt-16 text-lg text-center">
-      <span className="block">Welcome,<span className="font-bold mx-1">{session?.user?.email}</span>  </span>  <span className="text-green-300">
-       Supabase</span> Authentication Dahboard!
+        <span className="block">
+          Welcome,<span className="font-bold mx-1">{session?.user?.email}</span>{" "}
+        </span>{" "}
+        <span className="text-green-300">Supabase</span> Authentication
+        Dahboard!
       </h3>
-      <button onClick={handleSignOut} className="flex items-center p-2 bg-red-500 rounded-md">Sign Out</button>
-        
+      <button
+        onClick={handleSignOut}
+        className="flex items-center p-2 bg-red-500 rounded-md"
+      >
+        Sign Out
+      </button>
     </div>
   );
 };
