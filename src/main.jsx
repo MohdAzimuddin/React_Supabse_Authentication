44b47a813b88686ced3dc8ce52469950b73e3775
MohdAzimuddin <sfsuper2020@gmail.com>
2025-07-01 23:43:29 +0530

    updated main.jsx by removing the title added that into footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './routes/router.jsx'
-import { RouterProvider } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import { Power } from 'lucide-react'
-import { Toaster } from 'react-hot-toast'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./routes/router.jsx";
+import { RouterProvider } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { Toaster } from "react-hot-toast";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthContextProvider>
-            <div className="py-8 flex items-center justify-center gap-1">
-              <span className="text-green-600">
-                <Power/>
-              </span>
-              <span className="text-green-400 text-sm font-medium">
-                Powered by Supabase
-              </span>
-          </div>
-          <Toaster position='top-center' reverseOrder={false}/>
-<RouterProvider router={router}/>
+      <Toaster position="top-center" reverseOrder={false} />
+      <RouterProvider router={router} />
     </AuthContextProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
