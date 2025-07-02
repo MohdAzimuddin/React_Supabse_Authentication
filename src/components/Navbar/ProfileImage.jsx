490601d11fe1bb88ae5f667cdd1caf11a3643d65
MohdAzimuddin <sfsuper2020@gmail.com>
2025-07-02 14:35:52 +0530

    added new image feature  for user

---
+++ src/components/Navbar/ProfileImage.jsx
@@ -0,0 +1,12 @@
+import { UserCircle } from "lucide-react";
+import React from "react";
+
+const ProfileImage = ({ session }) => {
+  const imgUrl = session?.user?.user_metadata?.avatar_url;
+
+  return (
+    <div className="w-10 h-10 rounded-full overflow-hidden bg-gray-700 flex items-center justify-center">{imgUrl ? <img src={imgUrl} alt="user image" className="w-full h-full object-cover" /> : <UserCircle className="text-green-400 w-8 h-8" />}</div>
+  );
+};
+
+export default ProfileImage;
