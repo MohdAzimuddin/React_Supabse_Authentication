d55d2b7115e85ca56f596152d782ff3348c75318
MohdAzimuddin <sfsuper2020@gmail.com>
2025-07-03 02:21:23 +0530

    added user profile image component with fallback

--- src/components/Navbar/ProfileImage.jsx
+++ src/components/Navbar/ProfileImage.jsx
@@ -5,7 +5,17 @@ const ProfileImage = ({ session }) => {
   const imgUrl = session?.user?.user_metadata?.avatar_url;
 
   return (
-    <div className="w-10 h-10 rounded-full overflow-hidden bg-gray-700 flex items-center justify-center">{imgUrl ? <img src={imgUrl} alt="user image" className="w-full h-full object-cover" /> : <UserCircle className="text-green-400 w-8 h-8" />}</div>
+    <div className="w-12 h-12 rounded-full overflow-hidden bg-gray-700 flex items-center justify-center">
+      {imgUrl ? (
+        <img
+          src={imgUrl}
+          alt="user image"
+          className="w-full h-full object-cover"
+        />
+      ) : (
+        <UserCircle className="text-green-400 w-8 h-8" />
+      )}
+    </div>
   );
 };
 
