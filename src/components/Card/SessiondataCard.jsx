393a42aa103a275000d9e17a120b1e0dfc7d2eeb
MohdAzimuddin <sfsuper2020@gmail.com>
2025-07-03 16:16:53 +0530

    updating by adjusting some styles for better ux

--- src/components/Card/SessiondataCard.jsx
+++ src/components/Card/SessiondataCard.jsx
@@ -3,16 +3,16 @@ const SessiondataCard = ({ data }) => {
   return (
     <div className="w-full py-8 bg-gray-800 p-4 flex flex-col gap-4 rounded-xl shadow-sm shadow-green-400 hover:shadow-md hover:shadow-green-600">
       <div className="flex gap-2 items-center">
-        <span className="p-1 bg-gray-700 rounded-xl text-green-500">
+        <span className="p-1 bg-gray-700 rounded-xl text-green-500 text-xl lg:text-2xl">
           {Icon && <Icon />}
         </span>
-        <span>{data.name}</span>
+        <span className="text-lg lg:text-xl font-bold">{data.name}</span>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {data.rows.map((items, index) => (
           <div key={index}>
             <div>
-              <p className="md:px-2">{items.desc}</p>
+              <p className="md:px-2 text-sm sm:text-md lg:text-lg text-gray-300">{items.desc}</p>
               <p
                 className="mt-2 py-1 px-3 bg-zinc-700 truncate cursor-help"
                 title={items.fullData || items.data}
--- src/components/Card/UserdataCard.jsx
+++ src/components/Card/UserdataCard.jsx
@@ -1,24 +1,24 @@
 const UserdataCard = ({ data }) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 py-8 transition delay-500">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 py-8 transition delay-500 items-center">
       {data.map((item) => (
         <div
           key={item.id}
-          className="w-full p-1 xs:p-3 md:p-4 lg:p-6 bg-gray-800 rounded-xl flex flex-col gap-2 shadow-sm shadow-green-400 hover:shadow-md hover:shadow-green-600"
+          className="w-full py-4 md:py-6 px-3 md:px-4 lg:px-8 bg-gray-800 rounded-xl flex flex-col gap-2 shadow-sm shadow-green-400 hover:shadow-md hover:shadow-green-600 "
         >
           <div className="flex items-center gap-2 ">
-            <span className="p-1 bg-gray-700 rounded-xl text-green-500">
+            <span className="p-1 bg-gray-700 rounded-xl text-green-500 text-xl lg:text-2xl">
               <item.icon />
             </span>
-            <span className="text-md">{item.name}</span>
+            <span className="text-lg lg:text-xl font-bold">{item.name}</span>
           </div>
           <p
-            className="text-lg truncate cursor-help"
+            className="text-md sm:text-lg font-semibold truncate cursor-help"
             title={item.fullData || item.data}
           >
             {item.data}
           </p>
-          <p className="text-sm">{item.desc}</p>
+          <p className="text-sm sm:text-md lg:text-lg text-gray-300">{item.desc}</p>
         </div>
       ))}
     </div>
